6c17c29c01fd57bf053bdb902f165e4591087c1e
riteshsoni123 <comedybuddy1@gmail.com>
2022-02-16 01:24:28 +0530

    added the code to get to the next page

--- Codeforces/codeforces.py
+++ Codeforces/codeforces.py
@@ -1,4 +1,5 @@
 from audioop import cross
+from pydoc import cli
 from re import T
 from idna import valid_label_length
 from selenium import webdriver
@@ -15,114 +16,127 @@ def call_element(driver,xpath):
     try:
         current_element=ui.WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,xpath)))
     except:
-        print(xpath)
-        driver.quit()
+        if len(xpath)>52 and xpath[0:52]=='//*[@id="pageContent"]/div[4]/div[6]/table/tbody/tr[':
+            return 'next_page'
+        else:
+            print(xpath)
+            driver.quit()
     return current_element
 
+# //*[@id="pageContent"]/div[4]/div[6]/table/tbody/tr[2]/td[1]/a
+# //*[@id="pageContent"]/div[4]/div[6]/table/tbody/tr[51]/td[1]/a
 
 main_dir = "C:/Users/comed/Desktop/codeforces/"
 
 driver=webdriver.Chrome()
 
 # driver.get('https://codeforces.com/submissions/demo_user')
-driver.get('https://codeforces.com/submissions/Akash_Dixit27')
+driver.get('https://codeforces.com/submissions/ritesh_soni123')
 
 time.sleep(2)
 
-for i in range(2,29):
-    codeId=call_element(driver,'//*[@id="pageContent"]/div[4]/div[6]/table/tbody/tr['+str(i)+']/td[1]/a')
-    codeId.click()
 
-    names=call_element(driver,'//*[@id="facebox"]/div/div/div/span').text
-    print(names)
-    contestName=''
-    QuestionName=''
-    verdict='Wrong'
-    startIndex=-1
-    endIndex=-1
-    for j in range(len(names)):
-        if names[j]==':':
-            startIndex=startIndex=j+2
+for p in range(1,200):
+    if p>1:
+        driver.get('https://codeforces.com/submissions/ritesh_soni123/page/'+str(p))
 
-        if names[j]==',' and startIndex!=-1:
-            endIndex=j
-
-        if startIndex !=-1 and endIndex !=-1:
-            contestName=names[startIndex:endIndex]
-            break
-
-
-    startIndex=-1
-    endIndex=-1
-
-    for j in range(len(names)):
-        if names[j]==':' and names[j+2]=='(':
-            startIndex=j+2
-            
-        if names[j]==',' and startIndex!=-1:
-            endIndex=j
-
-        if startIndex !=-1 and endIndex !=-1:
-            QuestionName=names[startIndex:endIndex]
-            break
-
-    for j in range(len(names)-9):
-        if names[j:j+8] == 'Accepted':
-            verdict='Accepted'
-            break
     
-    unUsedCharacter=['\\','/',':','*','?','"','<','>','|']
+    for i in range(51,1000):
+        codeId=call_element(driver,'//*[@id="pageContent"]/div[4]/div[6]/table/tbody/tr['+str(i)+']/td[1]/a')
+        if codeId=='next_page':
+            break
+        codeId.click()
 
-    temp=''
-    for j in contestName:
-        flag=True
-        for k in unUsedCharacter:
-            if j==k:
-                flag=False
+        names=call_element(driver,'//*[@id="facebox"]/div/div/div/span').text
+        print(names)
+        contestName=''
+        QuestionName=''
+        verdict='Wrong'
+        startIndex=-1
+        endIndex=-1
+        for j in range(len(names)):
+            if names[j]==':':
+                startIndex=startIndex=j+2
+
+            if names[j]==',' and startIndex!=-1:
+                endIndex=j
+
+            if startIndex !=-1 and endIndex !=-1:
+                contestName=names[startIndex:endIndex]
                 break
-        if flag:
-            temp=temp+str(j)
 
-    contestName=temp
-    temp=''
 
-    for j in QuestionName:
-        flag=True
-        for k in unUsedCharacter:
-            if j==k:
-                flag=False
+        startIndex=-1
+        endIndex=-1
+
+        for j in range(len(names)):
+            if names[j]==':' and names[j+2]=='(':
+                startIndex=j+2
+                
+            if names[j]==',' and startIndex!=-1:
+                endIndex=j
+
+            if startIndex !=-1 and endIndex !=-1:
+                QuestionName=names[startIndex:endIndex]
                 break
-        if flag:
-            temp=temp+str(j)
 
-    QuestionName=temp
+        for j in range(len(names)-9):
+            if names[j:j+8] == 'Accepted':
+                verdict='Accepted'
+                break
+        
+        unUsedCharacter=['\\','/',':','*','?','"','<','>','|']
 
-    print(contestName)
-    print(QuestionName)
-    print(verdict)
+        temp=''
+        for j in contestName:
+            flag=True
+            for k in unUsedCharacter:
+                if j==k:
+                    flag=False
+                    break
+            if flag:
+                temp=temp+str(j)
 
-    if verdict == 'Wrong':
+        contestName=temp
+        temp=''
+
+        for j in QuestionName:
+            flag=True
+            for k in unUsedCharacter:
+                if j==k:
+                    flag=False
+                    break
+            if flag:
+                temp=temp+str(j)
+
+        QuestionName=temp
+
+        print(contestName)
+        print(QuestionName)
+        print(verdict)
+
+        if verdict == 'Wrong':
+            call_element(driver,'//*[@id="facebox"]/div/a/img').click()
+            print('***************************Page= '+str(p)+' Question= '+str(i)+'  ***************************')
+            continue
+
+        if not os.path.isdir(main_dir+contestName+"/"):
+            os.mkdir(main_dir+contestName)
+        
+        if os.path.isfile(main_dir+contestName+"/"+QuestionName+'.cpp'):
+            continue
+
+        f = open(main_dir+contestName+"/"+QuestionName+'.cpp',"a")
+
+        for j in range(1,1000):
+            try:
+                store=driver.find_element(By.XPATH,'//*[@id="facebox"]/div/div/div/pre/code/ol/li['+str(j)+']').text
+                f.write(store+"\n")
+            except:
+                break
+        f.close()
+        # //*[@id="facebox"]/div/a/img
         call_element(driver,'//*[@id="facebox"]/div/a/img').click()
-        print('******************************************  '+str(i)+'  ******************************************')
-        continue
-
-    if not os.path.isdir(main_dir+contestName+"/"):
-        os.mkdir(main_dir+contestName)
-    
-    if os.path.isfile(main_dir+contestName+"/"+QuestionName+'.cpp'):
-        continue
-
-    f = open(main_dir+contestName+"/"+QuestionName+'.cpp',"a")
-
-    for j in range(1,1000):
-        try:
-            store=driver.find_element(By.XPATH,'//*[@id="facebox"]/div/div/div/pre/code/ol/li['+str(j)+']').text
-            f.write(store+"\n")
-        except:
-            break
-    f.close()
-
-    call_element(driver,'//*[@id="facebox"]/div/a/img').click()
-    print('******************************************  '+str(i)+'  ******************************************')
+        print('***************************Page= '+str(p)+' Question= '+str(i)+'  ***************************')
 
 
--- Codeforces/demo.py
+++ Codeforces/demo.py
@@ -1,5 +1,9 @@
-unUsedCharacter=['\\','/',':','*','?','"','<','>','|']
-for i in unUsedCharacter:
-    print(i)
+# //*[@id="pageContent"]/div[9]/ul/li[1]/span/a
+# //*[@id="pageContent"]/div[4]/div[6]/table/tbody/tr[
+str='//*[@id="pageContent"]/div[4]/div[6]/table/tbody/tr['
+str2='//*[@id="pageContent"]/div[4]/div[6]/table/tbody/tr[1]/td[1]/a'
+print(len(str))
+print(str)
+print(str2[0:52])
 
 
